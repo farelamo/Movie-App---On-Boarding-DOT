f6798862b683dd5712ffd6dad4292e8583f46fda
Fariz Fahmi <farizfahmi674@gmail.com>
2022-04-08 03:11:33 +0700

    add studios entity

--- src/studios/studios.module.ts
+++ src/studios/studios.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Studios } from './studios.entity'
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([Studios])]
+})
 export class StudiosModule {}
