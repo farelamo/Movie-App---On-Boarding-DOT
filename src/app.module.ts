e4140db80d62527f3bd3816a4ee69db13f7cc2b0
Fariz Fahmi <farizfahmi674@gmail.com>
2022-04-08 01:17:02 +0700

    Refactor App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppService } from './app.service';
-import { TagsModule } from './tags/tags.module';
 import 'dotenv/config'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
-    TagsModule
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      logging: true,
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
