4784bdeb007a7b5d86be00e81213af395a89d395
Fariz Fahmi <farizfahmi674@gmail.com>
2022-04-08 02:50:07 +0700

    add order_items entity

--- src/order_items/order_items.module.ts
+++ src/order_items/order_items.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Orders_Items } from './order_items.entity'
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([Orders_Items])]
+})
 export class OrderItemsModule {}
